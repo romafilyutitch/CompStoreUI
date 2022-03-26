83a4a77e990b0ee126ee474bdb9e7beb3f22cc88
Roman Filiyutich <roman.filiyutich@sap.com>
2022-03-27 01:02:00 +0300

    Implement computer details page

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap} from "@angular/router";
+import {Computer} from "../model/computer";
+import {ComputerService} from "../service/computer.service";
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  computer: Computer | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private computerService: ComputerService) { }
+
+  ngOnInit(): void {
+    //First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('id'));
+
+    //Find the product that correspond with the id provided in route.
+    this.computerService.findById(productIdFromRoute)
+      .subscribe(response => {
+        this.computer = response;
+        console.log(this.computer);
+      });
+  }
+
+}
