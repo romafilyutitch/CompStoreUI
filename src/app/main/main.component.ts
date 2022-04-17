924d67b8eae31dab6dacabdb3cc32977b0af2e5a
Roman Filiyutich <roman.filiyutich@sap.com>
2022-04-17 18:46:02 +0300

    Add computer images on main page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,7 @@ import {ComputerService} from "../service/computer.service";
 export class MainComponent implements OnInit {
 
   computers: Computer[] = [];
+  computerImages: Map<Computer, any[]> = new Map<Computer, any[]>();
 
   constructor(private computerService: ComputerService) {
   }
@@ -18,14 +19,25 @@ export class MainComponent implements OnInit {
     this.computerService.findAll()
       .subscribe(response => {
         this.computers = response;
+        this.downloadImages();
       });
+  }
+
+  private downloadImages() {
+    this.computers.forEach(computer => {
+      this.computerService.downloadComputerImages(computer).subscribe(images => {
+        images = images.map(image => 'data:image/jpeg;base64,' + image.picByte);
+        this.computerImages.set(computer, images);
+        console.log(this.computerImages);
+      })
+    });
   }
 
   findByName(name: string): void {
     this.computerService.findByName(name)
       .subscribe(computers => {
         this.computers = computers;
+        this.downloadImages();
       })
   }
-
 }
